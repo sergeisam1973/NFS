f8e6e429a1a0c222ef20ecdddae47cda05d119cb
sergeisam1973 <sergeisam@mail.ru>
2024-02-15 14:52:41 +0300

    «commit_1»

---
+++ nfss_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+yum install nfs-utils
+systemctl enable firewalld --now
+firewall-cmd --add-service="nfs3" --add-service="rpc-bind" --add-service="mountd" --permanent
+firewall-cmd --reload
+systemctl enable nfs --now
+mkdir -p /srv/share/upload
+chown -R nfsnobody:nfsnobody /srv/share
+chmod 0777 /srv/share/upload
+cat << EOF > /etc/exports
+/srv/share 192.168.50.11/32(rw,sync,root_squash)
+EOF
+exportfs -r
+
